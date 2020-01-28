d0f72ba45a9a18dffacab1fdaba4eb11cc79b454
Arun6153 <sarun6153@gmail.com>
2020-01-28 20:06:47 +0530

    DB-Online

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/quizBuilder", { useNewUrlParser: true ,useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://ArunSaini:arun6153@cluster0-vensc.mongodb.net/quizBuilder", { useNewUrlParser: true ,useUnifiedTopology: true})
 .then(() => {
     console.log("Database Connected!");
 })
